2c3ac70521192b2d7286233d9df5e61fbaf5c951
jfchicaiza <jf.chicaiza@soprasteria.com>
2023-04-24 10:16:47 +0200

    fix(game-view): mock player helper

--- test/unit/Game/views/gameView.test.js
+++ test/unit/Game/views/gameView.test.js
@@ -3,9 +3,21 @@ import { fixture, expect, assert, aTimeout } from '@open-wc/testing'
 import sinon from 'sinon'
 import { Router } from '@vaadin/router'
 import '../../../../src/Game/views/gameView.js'
+import { PlayerHelper } from '../../../../src/shared/helpers/playerHelper.js'
 
 describe('GameView', () => {
   let element
+
+  function fn() {}
+
+  sinon.replace(PlayerHelper, 'getCurrentPlayer', sinon.fake(fn))
+  sinon.replace(
+    PlayerHelper,
+    'getPlayer',
+    sinon.fake(() => ({ score: 0, highScore: 0, userName: 'olo' }))
+  )
+  sinon.replace(PlayerHelper, 'updatePlayer', sinon.fake(fn))
+  sinon.replace(PlayerHelper, 'removeCurrentPlayer', sinon.fake(fn))
 
   beforeEach(async () => {
     element = await fixture(html`<game-view></game-view>`)
