48a252c0f74bbce16efc461cbbcff2e0cf5bcb72
jfchicaiza <jf.chicaiza@soprasteria.com>
2023-05-02 15:58:59 +0200

    feat(ranking-view): navigate to home #HU03-RANKING

--- src/Ranking/views/rankingView.js
+++ src/Ranking/views/rankingView.js
@@ -1,7 +1,18 @@
 import { LitElement, html, css } from 'lit'
+import { Router } from '@vaadin/router'
+import { View } from '../../shared/constants/view.js'
 import '../components/rankingTableComponent.js'
 
+const exitIconSrc = new URL(
+  '../../../assets/right-from-bracket-solid.svg',
+  import.meta.url
+).href
+
 class RankingView extends LitElement {
+  static _emitExit() {
+    Router.go({ pathname: View.Home.id })
+  }
+
   render() {
     return html`
       <div id="ranking-containter">
@@ -10,6 +21,13 @@ class RankingView extends LitElement {
         </div>
         <ranking-table-component></ranking-table-component>
       </div>
+      <img
+        class="exit-icon"
+        alt="exit-icon"
+        src=${exitIconSrc}
+        @click=${RankingView._emitExit}
+        tabindex="0"
+      />
     `
   }
 
@@ -33,6 +51,18 @@ class RankingView extends LitElement {
 
     ranking-table-component {
       display: inline-block;
+    }
+
+    .exit-icon {
+      position: fixed;
+      height: 25px;
+      top: 22px;
+      right: 5%;
+      filter: invert(100%);
+    }
+
+    .exit-icon:hover {
+      filter: invert(80%);
     }
   `
 }
